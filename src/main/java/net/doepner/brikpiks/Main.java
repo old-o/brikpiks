7e78dcacd0925de98934f57965a6f3e8116b6b04
odoepner <odoepner@gmail.com>
2017-04-02 11:01:09 -0300

    Add a pointer to some code examples

--- src/main/java/net/doepner/brikpiks/Main.java
+++ src/main/java/net/doepner/brikpiks/Main.java
@@ -7,6 +7,9 @@ public final class Main {
 
     public static void main(String[] args) {
         System.out.println("Brik Piks");
+
+        // TODO : Get started by reviewing
+        // http://stackoverflow.com/questions/15777821/how-can-i-pixelate-a-jpg-with-java
     }
 
 }
