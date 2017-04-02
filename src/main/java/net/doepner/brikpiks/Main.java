ab41101d4469cd43709f83c3ce86943776baaab4
odoepner <odoepner@gmail.com>
2017-04-02 10:58:02 -0300

    Initial commit

---
+++ src/main/java/net/doepner/brikpiks/Main.java
@@ -0,0 +1,12 @@
+package net.doepner.brikpiks;
+
+/**
+ * Application entry point
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Brik Piks");
+    }
+
+}
