c09b0389701d41273d79552ae5e17ce619d92525
odoepner <odoepner@gmail.com>
2017-04-02 20:07:10 -0300

    Add a pointer to some code examples

--- src/main/java/net/doepner/brikpiks/Main.java
+++ src/main/java/net/doepner/brikpiks/Main.java
@@ -10,6 +10,8 @@ public final class Main {
 
         // TODO : Get started by reviewing
         // http://stackoverflow.com/questions/15777821/how-can-i-pixelate-a-jpg-with-java
+        // http://stackoverflow.com/questions/25761438/understanding-bufferedimage-getrgb-output-values
+        // http://stackoverflow.com/questions/28162488/get-average-color-on-bufferedimage-and-bufferedimage-portion-as-fast-as-possible
     }
 
 }
